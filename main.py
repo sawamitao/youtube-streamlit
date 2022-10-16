b293e4158e2970c943c111201a044d570b00fd0c
sawami.tao <sawami.tao.303@gmail.com>
2022-10-16 13:09:51 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from PIL import Image
+import time
+
+
+st.title("Streamlit 超入門")
+
+st.write("プログレスバーの表示")
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+for i in range(100):
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+
+
+left_column,right_column = st.beta_columns(2)
+button = left_column.button("右カラムに文字を表示")
+if button:
+    right_column.write("ここは右カラム")
+expander = st.beta_expander('問い合わせ')
+expander.write("問い合わせ内容を書く")
+text = st.sidebar.text_input("あなたの趣味を教えてください")
+
+# option = st.selectbox(
+#     'あなたが好きな数字を教えてください',
+#     list(range(1,11))
+# )
+'あなたの趣味',text
+
+condition = st.sidebar.slider('あなたの今の調子は',0,100,50)
+
+# if st.checkbox("show Image"):
+#     img = Image.open('instagrm_icon.png')
+#     st.image(img,caption = "Instagram",use_column_width = True)
+# df = pd.DataFrame(
+#     np.random.rand(100,2)/[50,50] + [35.69,139.70],
+#     columns=['lat','lon']
+# )
+# # st.write(df)
+# # st.dataframe(df)
+# st.map(df)
+"""
+# 章
+## 節
+### 項
+```
+import pandas as pd
+```
+"""
+
